d7faa509294da5b941242d3332363054b62165c4
unknown <aotwbys@gmail.com>
2020-09-30 16:54:58 -0400

    working loginlogout

--- routes/web/home.js
+++ routes/web/home.js
@@ -1,76 +1,73 @@
-//holds all of the routes.
-
 var express = require("express");
-//passport to authenticate user, check if user exists
 var passport = require("passport");
 
 var User = require("../../models/user");
 
 var router = express.Router();
 
-router.get("/", function(req, res){
-    console.log("We're at the basic view here.")
-    //Render index view later
-    res.render("home/index");
+
+router.get("/", function (req, res) {
+   // console.log("hello I'm on the start page");
+   res.render("home/");
 });
 
 router.get("/home", function (req, res) {
-    res.render("home/home");
-});
-
-router.get("/about", function (req, res) {
-    res.render("home/about");
+   res.render("home/home");
 });
 
 router.get("/self", function (req, res) {
     res.render("home/self");
 });
 
+router.get("/about", function (req, res) {
+   res.render("home/about");
+});
+
 router.get("/login", function (req, res) {
-    res.render("home/login")
- });
- 
- router.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/home");
- });
- 
- router.post("/login", passport.authenticate("login", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true
- }));
- 
- router.get("/signup", function (req, res) {
-    res.render("home/signup")
- });
- 
- router.post("/signup", function (req, res, next) {
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
- 
-    User.findOne({ email: email }, function (err, user) {
-       if (err) { return next(err); }
-       if (user) {
-          req.flash("error", "There's already an account with this email");
-          return res.redirect("/signup");
-       }
- 
-       var newUser = new User({
-          username: username,
-          password: password,
-          email: email
-       });
- 
-       newUser.save(next);
- 
-    });
- 
- }, passport.authenticate("login", {
-    successRedirect: "/",
-    failureRedirect: "/signup",
-    failureFlash: true
- }));
- 
- module.exports = router;
+   res.render("home/login")
+});
+
+router.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/home");
+});
+
+router.post("/login", passport.authenticate("login", {
+   successRedirect: "/",
+   failureRedirect: "/login",
+   failureFlash: true
+}));
+
+router.get("/signup", function (req, res) {
+   res.render("home/signup")
+});
+
+router.post("/signup", function (req, res, next) {
+   var username = req.body.username;
+   var email = req.body.email;
+   var password = req.body.password;
+
+   User.findOne({ email: email }, function (err, user) {
+      if (err) { return next(err); }
+      if (user) {
+         req.flash("error", "There's already an account with this email");
+         return res.redirect("/signup");
+      }
+
+      var newUser = new User({
+         username: username,
+         password: password,
+         email: email
+      });
+
+      newUser.save(next);
+
+   });
+
+}, passport.authenticate("login", {
+   successRedirect: "/",
+   failureRedirect: "/signup",
+   failureFlash: true
+}));
+
+module.exports = router;
